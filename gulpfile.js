f17d30fa9616afb3dd369fb4063bc415d1c0def7
Daniel Dolinchuk <fixten@yandex.ru>
2017-05-26 13:10:58 +0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require ('gulp'),
+  concat = require('gulp-concat');
+    less = require('gulp-less');
+   debug = require('gulp-debug');
+   paths = {
+     less: ['components/global/*.less'],
+     blocks: ['components/blocks/*.less'],
+     styles: ['styles/styles.less']
+   };
+
+  gulp.task('build', function() {
+    return gulp.src(['components/global/*.less', 'components/blocks/*.less'])
+      .pipe(concat('styles.less', {newLine: '\n'}))
+      .pipe(gulp.dest('styles'))
+      .pipe(debug());
+  });
+
+  gulp.task('less', function () {
+  return gulp.src('styles/styles.less')
+    .pipe(less())
+    .pipe(gulp.dest('styles'))
+    .pipe(debug());
+});
+
+  gulp.task('watch', function() {
+    gulp.watch(paths.less, ['build']);
+    gulp.watch(paths.blocks, ['build']);
+    gulp.watch(paths.styles, ['less']);
+});
+
+gulp.task('default', ['build', 'less', 'watch']);
